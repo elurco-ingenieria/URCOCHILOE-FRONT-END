49c01f2e031da5720198ef0b750e50d41d041e51
danielpinaud <danielpinaud001@gmail.com>
2025-05-06 22:13:58 -0400

    06

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -8,13 +8,13 @@ const contenido = [
     },
     {
         imagen: 'https://scontent.fyai4-1.fna.fbcdn.net/v/t51.75761-15/480775261_18131277382400997_7826167708746121668_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeGoHCRCjpRBCXzfxTeJmoVLmRb0YCXi1niZFvRgJeLWeMWHwuomxyZkQolOVHIItEdv-Q49bgVe5YFJrXRfFdL4&_nc_ohc=2T2KH59cD6sQ7kNvwFtRP2Z&_nc_oc=AdlvUyeRuK7LlLIexycg9yFSYT39avzbrrq7ll74KwR9LGrp_pCX7fraWRRXDNZQY9A&_nc_zt=23&_nc_ht=scontent.fyai4-1.fna&_nc_gid=vAjL2XyAizLv8Cx7OfYNcw&oh=00_AfKCauc_1LHvHslQHLma3tMkBT-nG18gUE7YlgMf_FHz1Q&oe=681F3331',
-        titulo: '¡Bienvenido!',
-        descripcion: '¡Disfruta de nuestras Alojamientos 🌿!',
+        titulo: ' ¡Bienvenido A Ancud - Isla Chiloé!',
+        descripcion: '¡Disfruta de nuestros Alojamientos 🌿!',
     },
     {
-        imagen: 'https://scontent.fyai4-1.fna.fbcdn.net/v/t39.30808-6/473672580_122116298426616243_5082192132296299461_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeFJUPawwGtLNbFapx7GsLrewTycRWgS6QHBPJxFaBLpAWvBrKbYS8EW70IahNX50GiBLsoGh7iJWkpgOT-bq-qw&_nc_ohc=O5oioZSV0V0Q7kNvwG00N6G&_nc_oc=Adl5klsU-hFp2ZD3-ErVvS50TyCWscUFpLppoN8i1-w68ptUmT2kzbPZU7BrAlTTqrA&_nc_zt=23&_nc_ht=scontent.fyai4-1.fna&_nc_gid=JflQG2EWOSGIJ91wxTGIzA&oh=00_AfIqwIxOvFFPfEjrrEpj4JjXsVKsZoreOwYSkoCnJvsyfA&oe=681F5A42',
-        titulo: '¡Bienvenido!',
-        descripcion: '¡Disfruta de nuestras Alojamientos ✨', 
+        imagen: 'https://scontent.fyai4-1.fna.fbcdn.net/v/t39.30808-6/478842078_936096168681909_4905114757681202818_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeEhCsRjT8Y3TxMagjBBSF7E8X-E8knKJTnxf4TyScolObftofFxFedNRLzMm5UVP7ZsaMIiyr4aH_Jk-mkEokn4&_nc_ohc=vkUM4annZFEQ7kNvwEAjohu&_nc_oc=AdktwreMdvJ7A_4vWr2TngbeiIgs8u3sZPXLicOg4ya2s5FAUMo-flLKJolt4u-BYjc&_nc_zt=23&_nc_ht=scontent.fyai4-1.fna&_nc_gid=URH7BwBq7eedCITHGzg0UA&oh=00_AfLNpLv3H3W4o6SEmcUGTWN5-SfXVanN6kbWbQFPDo92sg&oe=68207736',
+        titulo: ' ¡Bienvenido A Ancud - Isla Chiloé!',
+        descripcion: '¡Disfruta de nuestros Alojamientos 🌿!',
     },
 ];
 
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -11,7 +11,7 @@ const apiURL = import.meta.env.VITE_API_URL;
 
 const About = () => {
     const comentarios = [
-        {
+      /*  {
             nombre: "Juan Pérez",
             comentario: "¡Excelente servicio! Mi experiencia en Chiloé fue inolvidable gracias a Alojamientos Rokadan.",
             imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRExvkt9t5ziKNqqRY0kmqMVRJiyYdiwPNskw&s", // ruta de la imagen
@@ -26,33 +26,28 @@ const About = () => {
             comentario: "Recomiendo totalmente este servicio. Todo fue impecable y muy profesional.",
             imagen: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPu7PtWmoU2qTAAwE3DfdLfCIXn9VS-0i13Q&s", // ruta de la imagen
         },
+        */
     ];
 
     return (
         <div style={{ padding: "20px", maxWidth: "800px", margin: "0 auto", textAlign: "center" }}>
             <h1 style={{ marginBottom: "20px", color: "#1F7D53" }}>Quiénes Somos</h1>
             <p style={{ fontSize: "18px", lineHeight: "1.6", color: "#333" }}>
-                Bienvenido a Alojamientos Rokadan, donde la belleza de Chiloé se encuentra con la comodidad y el estilo.
-                Somos la conexión perfecta entre los viajeros y los diversos alojamientos que la región tiene para ofrecer.
-            </p>
-            <p style={{ fontSize: "18px", lineHeight: "1.6", color: "#333" }}>
-                Somos una empresa dedicada a ofrecer las mejores opciones de alojamiento en la región de Chiloé. 
-                Nuestro objetivo es conectar a los viajeros con alojamientos únicos, cómodos y accesibles, 
-                mientras promovemos el turismo local y apoyamos a los pequeños negocios de la zona.
-            </p>
-            <p style={{ fontSize: "18px", lineHeight: "1.6", color: "#333" }}>
-                Creemos en la importancia de brindar experiencias inolvidables a nuestros clientes, 
-                ofreciendo un servicio personalizado y de alta calidad. Nuestro equipo está comprometido 
-                con la excelencia y trabaja arduamente para garantizar que cada estancia sea especial.
-            </p>
-            <p style={{ fontSize: "18px", lineHeight: "1.6", color: "#333" }}>
-                Gracias por confiar en nosotros y permitirnos ser parte de tus aventuras en Chiloé.
-            </p>
+            Bienvenidos a Urco Chiloé
+
+                En el corazón de Ancud, Chiloé, nace Urco Chiloé, un espacio acogedor que reúne la calidez del sur con la magia del archipiélago. Somos una empresa familiar dedicada a brindar experiencias únicas de descanso y conexión con la naturaleza, a través de nuestras cómodas cabañas equipadas y nuestro encantador hostal con atención personalizada.
+
+                Ubicados en un entorno privilegiado, rodeados de paisajes típicos chilotes y a pasos de atractivos turísticos, en Urco Chiloé nos comprometemos a ofrecer un servicio cercano, seguro y de calidad, ideal tanto para viajeros que buscan desconexión como para quienes exploran Chiloé por trabajo, cultura o aventura.
+
+                Ya sea que vengas en pareja, en familia o con amigos, te invitamos a vivir la autenticidad de Chiloé con nosotros.
+                Urco Chiloé: Descanso con alma chilota.            </p>
+            
             <img 
-                src="https://octaviaviajando.com/wp-content/uploads/2021/11/castro_palafitos_octaviaviajando.jpg" 
+                src="https://scontent.fyai4-1.fna.fbcdn.net/v/t39.30808-6/465800829_122094452792616243_4934034915940496865_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeF5C889GNIMgEY4F-zYs3cTEexo1BhCKRkR7GjUGEIpGWgQFYjbLMLN5go7HgafAygwqelxByl5e3CFUJ5ajucs&_nc_ohc=07UaWgeYiGoQ7kNvwG404u9&_nc_oc=AdnXpocqaJmJZ7k5nJFA6COZWZ5ZgjGlCveF-1IeFNdKbgyH-Wq09BzQHGQBip0opCw&_nc_zt=23&_nc_ht=scontent.fyai4-1.fna&_nc_gid=G8mrz2BfdUmG9HQJlfUxXg&oh=00_AfJXpKJtsW9h09hYw7rrwf5QZzUU2XTSKuNZFrHk5hVJsg&oe=68208DE7" 
                 alt="Imagen representativa de Chiloé" 
                 style={{ marginTop: "20px", width: "100%", maxWidth: "600px", borderRadius: "8px" }}
             />
+            
 
             {/* Sección de comentarios */}
             <div style={{ marginTop: "40px" }}>
